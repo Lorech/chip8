4a5fdf995c67b9700c49e524e9f9a07a2d8dde6e
Lorech <laurisdedumets@gmail.com>
2025-10-19 22:18:23 +0300

    feat: Link Raylib to project
    Also uses one of their example projects as a replacement for the Hello,
    World main function we have been using this entire time

--- src/main.c
+++ src/main.c
@@ -1,5 +1,16 @@
-#include <stdio.h>
+#include "raylib.h"
 
 int main(int argc, char **argv) {
-    printf("Hello, World!");
+    InitWindow(800, 450, "raylib [core] example - basic window");
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
 }
