3f2ac977bfb31cc9379f8c7ac86aa05cc718e32a
Lorech <laurisdedumets@gmail.com>
2025-10-21 10:04:49 +0300

    fix: Update tests to match new CPU functionality
    Also expands some of the existing tests to more accurately check system
    state

--- test/test_chip8.c
+++ test/test_chip8.c
@@ -1,60 +1,71 @@
 #include "chip8.h"
 #include "font.h"
-#include "memory.h"
-#include "stack.h"
-#include "timer.h"
 #include "unity_fixture.h"
 
 TEST_GROUP(CHIP8);
 
-static chip8_t     chip8;
-static memory_t    memory;
-static chipstack_t stack;
-static display_t   display;
-static timer_t     timer;
+static chip8_t chip8;
 
 TEST_SETUP(CHIP8) {
-    memory  = memory_create();
-    stack   = stack_create(4);
-    display = display_create();
-
-    timer_init(&timer);
-    chip8_init(&chip8, &memory, &stack, &timer, &display);
+    chip8_init(&chip8);
 }
 
-TEST_TEAR_DOWN(CHIP8) {
-    stack_destroy(&stack);
+TEST_TEAR_DOWN(CHIP8) {}
+
+TEST(CHIP8, LoadValidFont) {
+    // chip8_init loads the default font, which can be configured when
+    // compiling, so we need to pull the starting values to compare against.
+    font_type_t old_font   = chip8.font;
+    uint8_t    *old_memory = &chip8.ram[FONT_START];
+
+    uint8_t zero[5] = {0xE0, 0xA0, 0xA0, 0xA0, 0xE0};
+    bool    success = chip8_load_font(&chip8, FONT_DREAM6800);
+    TEST_ASSERT_TRUE_MESSAGE(success, "Loading valid font should not fail.");
+    TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(zero, &chip8.ram[FONT_START], sizeof(zero), "Loaded font should update memory.");
+    TEST_ASSERT_EQUAL_UINT8_MESSAGE(FONT_DREAM6800, chip8.font, "Loaded font should be set as active font");
 }
 
-TEST(CHIP8, LoadFont) {
-    bool dream_success = chip8_load_font(&chip8, FONT_DREAM6800);
-    TEST_ASSERT_TRUE(dream_success);
-    uint8_t dream_zero[5] = {0xE0, 0xA0, 0xA0, 0xA0, 0xE0};
-    TEST_ASSERT_EQUAL_UINT8_ARRAY(dream_zero, &chip8.memory->data[FONT_START], sizeof(dream_zero));
-    TEST_ASSERT_EQUAL_UINT8(FONT_DREAM6800, chip8.font);
+TEST(CHIP8, LoadInvalidFont) {
+    // chip8_init loads the default font, which can be configured when
+    // compiling, so we need to pull the starting values to compare against.
+    font_type_t old_font   = chip8.font;
+    uint8_t    *old_memory = &chip8.ram[FONT_START];
 
-    bool cosmac_success = chip8_load_font(&chip8, FONT_COSMACVIP);
-    TEST_ASSERT_TRUE(cosmac_success);
-    uint8_t cosmac_zero[5] = {0xF0, 0x90, 0x90, 0x90, 0xF0};
-    TEST_ASSERT_EQUAL_UINT8_ARRAY(cosmac_zero, &chip8.memory->data[FONT_START], sizeof(cosmac_zero));
-    TEST_ASSERT_EQUAL_UINT8(FONT_COSMACVIP, chip8.font);
+    bool success = chip8_load_font(&chip8, FONT_COUNT);
+    TEST_ASSERT_FALSE_MESSAGE(success, "Loading invalid font should fail.");
+    TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(old_memory, &chip8.ram[FONT_START], 5, "Ignored font should not update memory.");
+    TEST_ASSERT_EQUAL_UINT8_MESSAGE(old_font, chip8.font, "Ignored font should nont update active font");
 }
 
 TEST(CHIP8, LoadValidProgram) {
-    // NOTE: Not a valid CHIP-8 program; copied from font testing.
-    uint8_t  program[5] = {0xE0, 0xA0, 0xA0, 0xA0, 0xE0};
-    uint16_t size       = 5;
-    chip8.PC            = 0x204; // Allow for validating reset procedure.
+    uint8_t program[MEMORY_SIZE - PROGRAM_START] = {0x00, 0xE0};
+    uint8_t empty_vars[16]                       = {0};
+    // Change state so we can validate reset procedure.
+    chip8.pc          = 0x204;
+    chip8.i           = 0x500;
+    chip8.delay_timer = 0x20;
+    chip8.sound_timer = 0x20;
+    chip8.v[1]        = 0x40;
 
-    bool success = chip8_load_program(&chip8, program, size);
-    TEST_ASSERT_TRUE(success);
-    TEST_ASSERT_EQUAL_UINT8(PROGRAM_START, chip8.PC);
-    TEST_ASSERT_EQUAL_UINT8_ARRAY(program, &chip8.memory->data[chip8.PC], size);
+    bool success = chip8_load_program(&chip8, program, sizeof(program));
+    TEST_ASSERT_TRUE_MESSAGE(success, "Loading valid program should not fail.");
+    TEST_ASSERT_EQUAL_UINT8_MESSAGE(PROGRAM_START, chip8.pc, "PC should reset if loading succeeded.");
+    TEST_ASSERT_EQUAL_UINT8_MESSAGE(0, chip8.i, "I should reset if loading succeeded.");
+    TEST_ASSERT_EQUAL_UINT8_MESSAGE(0, chip8.delay_timer, "Delay timer should reset if loading succeeded.");
+    TEST_ASSERT_EQUAL_UINT8_MESSAGE(0, chip8.sound_timer, "Sound timer should reset if loading succeeded.");
+    TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(empty_vars, &chip8.v, sizeof(empty_vars), "Variables should reset if loading succeeded.");
+    TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(program, &chip8.ram[PROGRAM_START], sizeof(program), "Memory should contain the new program if loading succeeded.");
 }
 
 TEST(CHIP8, LoadInvalidProgram) {
-    uint8_t empty[MEMORY_SIZE] = {0};
-    chip8.PC                   = 0x204; // Allow for validating reset procedure.
+    uint8_t empty_program[MEMORY_SIZE - PROGRAM_START] = {0};
+    uint8_t expected_vars[16]                          = {0x00, 0x40};
+    // Change state so we can validate reset procedure.
+    chip8.pc          = 0x204;
+    chip8.i           = 0x500;
+    chip8.delay_timer = 0x20;
+    chip8.sound_timer = 0x20;
+    chip8.v[1]        = 0x40;
 
     // Intentionally create a program that spans entire memory range to test overflow handling.
     // Switch some bytes of the program to test memory mutation not happening on overflow.
@@ -66,17 +77,21 @@ TEST(CHIP8, LoadInvalidProgram) {
     program[1032]                = 0xE0;
 
     bool success = chip8_load_program(&chip8, program, sizeof(program));
-    TEST_ASSERT_FALSE(success);
-    TEST_ASSERT_EQUAL_UINT8(0x204, chip8.PC);
-    TEST_ASSERT_EQUAL_UINT8_ARRAY(empty, &chip8.memory->data[chip8.PC], sizeof(MEMORY_SIZE - chip8.PC));
+    TEST_ASSERT_FALSE_MESSAGE(success, "Loading invalid program should fail.");
+    TEST_ASSERT_EQUAL_UINT8_MESSAGE(0x204, chip8.pc, "PC should not reset if loading failed.");
+    TEST_ASSERT_EQUAL_UINT8_MESSAGE(0x500, chip8.i, "I should not reset if loading failed.");
+    TEST_ASSERT_EQUAL_UINT8_MESSAGE(0x20, chip8.delay_timer, "Delay timer should not reset if loading failed.");
+    TEST_ASSERT_EQUAL_UINT8_MESSAGE(0x20, chip8.sound_timer, "Sound timer should not reset if loading failed.");
+    TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(expected_vars, &chip8.v, sizeof(expected_vars), "Variables should not reset if loading failed.");
+    TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(empty_program, &chip8.ram[PROGRAM_START], sizeof(empty_program), "Memory should not change if loading failed.");
 }
 
 TEST(CHIP8, RunCycle) {
-    uint8_t       program[2] = {0xAA, 0xBB};
+    uint8_t       program[2] = {0x00, 0xE0};
     bool          loaded     = chip8_load_program(&chip8, program, sizeof(program));
     chip8_state_t result     = chip8_run_cycle(&chip8);
 
-    TEST_ASSERT_EQUAL_UINT8_MESSAGE(CHIP8_INSTRUCTION_NOT_IMPLEMENTED, result.status, "All chip8 instructions should be unimplemented");
-    TEST_ASSERT_EQUAL_UINT16_MESSAGE(0xAABB, result.opcode, "Fetched opcode should join two bytes together");
-    TEST_ASSERT_EQUAL_UINT16_MESSAGE(PROGRAM_START + 2, chip8.PC, "Valid fetch should advance the program counter by 2");
+    TEST_ASSERT_EQUAL_UINT16_MESSAGE(0x00E0, result.opcode, "Should create \"Clear Screen\".");
+    TEST_ASSERT_EQUAL_UINT8_MESSAGE(CHIP8_OK, result.status, "Should be implemented.");
+    TEST_ASSERT_EQUAL_UINT16_MESSAGE(PROGRAM_START + 2, chip8.pc, "Should advance PC.");
 }
